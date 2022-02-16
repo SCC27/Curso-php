3152e45884e06836d2bdfba22989d0adbc37d27d
SCC27 <solcisneros962705@gmail.com>
2022-02-16 00:01:05 -0600

    Ejecutando código y archivos PHP

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+//echo "Hola, estoy programando con php ";
+
+//$nombre= "Carlos";
+//$apellido= "Santana";
+
+//echo "No, no. Yo me llamo " . $nombre . " " . $apellido ;
+
+//echo "No, no. Yo me llamo $nombre $apellido";
+
+//echo "EL resoltado de 4 x 5 es " . 4*5.;
+
+$personas =[
+    "Carlos " => 22,
+    "Mr.Michi" => 15,
+    "Juand" => 65
+];
+
+//var_dump($personas);
+//print_r( $personas);
+
